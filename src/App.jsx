8670e94d059df218df77e3c45124f5dea4c4b468
Cr1605 <allyaercc@gmail.com>
2023-05-20 13:19:48 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Header from "./component/Header";
+
 const App = () => {
   //don't forget to add font link in index.html
-  return <h1>Subscribe Zainkeepscode</h1>;
+  return (
+    <Header />
+  )
 };
 
 export default App;
