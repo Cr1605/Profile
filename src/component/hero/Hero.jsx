2a95c5e5b732be2d335a95233eda4aab4c14de70
Cr1605 <allyaercc@gmail.com>
2023-05-21 23:18:15 +0700

    complete hero

--- src/component/hero/Hero.jsx
+++ src/component/hero/Hero.jsx
@@ -1,31 +1,57 @@
 import React from 'react'
 import css from './Hero.module.scss'
+import {motion} from 'framer-motion'
+import {fadeIn, slideIn, staggerContainer} from '../../utils/motion'
 
 const Hero = () => {
   return (
     <section className={`paddings ${css.wrapper}`}>
-        <div className={`innerWidth ${css.container}`}>
+        <motion.div 
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{once: false, amount: 0.25}}
+        
+        className={`innerWidth ${css.container}`}>
             {/* upperElements */}
-            <div className="upperElements">
-                <span>Hey There, <br/> I'm Ha.</span>
-                <span>I design beautiful simple <br /> things, And I love what i do</span>
+            <div className={css.upperElements}>
+                <motion.span 
+                variants={fadeIn("right", "tween", 0.2, 1)}
+                
+                className='primaryText'>Hey There, <br/> I'm Ha.</motion.span>
+                <motion.span 
+                variants={fadeIn("left", "tween", 0.4, 1)}
+                className='secondaryText'>I design beautiful simple <br /> things, And I love what i do</motion.span>
             </div>
+
+            <motion.div 
+            variants={fadeIn("up", "tween", 0.3, 1)}
+            className={css.person}>
+                <motion.img 
+                variants={slideIn("up", "tween", 0.5, 1.3)}
+                src="./person.png" alt="" />
+            </motion.div>
+
             {/* lowerElements */}
-            <div className="lowerElements">
-                <div className="experience">
+            <div className={css.lowerElements}>
+                <motion.div 
+                variants={fadeIn("right", "tween", 0.2, 1)}
+                className={css.experience}>
                     <div className="primaryText">10</div>
                     <div className="secondaryText">
                         <div>Year</div>
                         <div>Experience</div>
                     </div>
-                </div>
-                <div className="certificate">
+                </motion.div>
+                <motion.div 
+                variants={fadeIn("left", "tween", 0.4, 1)}
+                className={css.certificate}>
                     <img src="./certificate.png" alt="" />
-                    <span></span>
-                    <span></span>
-                </div>
+                    <span>CERTIFIED PROFATIONAL</span>
+                    <span>UI/UX DESIGNER</span>
+                </motion.div>
             </div>
-        </div>
+        </motion.div>
     </section>
   )
 }
